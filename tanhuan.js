7133e640f31fa939926471d67e35ce70f2da56a5
jackyHB <54207960+jackyHB@users.noreply.github.com>
2021-04-10 07:08:19 +0800

    Update tanhuan.js

--- tanhuan.js
+++ tanhuan.js
@@ -1,17 +1,11 @@
 [mitm]
 hostname= awmattack.ldxfej.com
-var body = {
-  "status" : "200",
-  "msg" : "返回成功",
-  "data" : {
-    "mobile" : "18866889999",
-    "status" : 1,
-    "is_vip" : 1,
-    "member_name" : "作者H.  ",
-    "head_pic" : "https://s3.ax1x.com/2021/01/26/sOLQKA.jpg",
-    "parent_id" : 22,
-    "user_viptime" : "2066-06-06"
-  }
-};
 
-$done({body:JSON.stringify(body)});
+body = $response.body.replace(/vip":0/g, 'vip":1').
+replace(/vip_expire":0/g, 'vip_expire":"2022-01-01"').
+replace(/coin":0/g, 'coin":888').
+replace(/qp_money":0/g, 'qp_money":999').
+replace(/short_watch_time":0/g, 'short_watch_time":2022-12-22').
+replace(/watch_time:0/g, 'watch_time":2023-2-12)
+
+$done({body});
